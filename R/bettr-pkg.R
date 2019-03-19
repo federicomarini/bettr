c3303550c7f4448429e38d2bb08a9df97e24d19b
Federico Marini <marinif@uni-mainz.de>
2019-03-19 16:34:49 +0100

    added bettr-pkg file for general pkg info and docs

---
+++ R/bettr-pkg.R
@@ -0,0 +1,7 @@
+#' bettr: a better way to explore what is best
+#' 
+#' @author Charlotte Soneson \email{charlottesoneson@@gmail.com}
+#' @author Federico Marini \email{marinif@@uni-mainz.de}
+#' @name bettr-pkg
+#' @docType package
+NULL
