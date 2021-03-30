a7cfda1be71d97a0630308b176823989f81c0b5a
Charlotte Soneson <charlottesoneson@gmail.com>
2021-03-30 11:08:22 +0200

    Don't use regexps in tests

--- tests/testthat/test-bettr.R
+++ tests/testthat/test-bettr.R
@@ -6,57 +6,75 @@ test_that("bettr stops with invalid inputs", {
     
     ## df
     expect_error(bettr(df = as.matrix(df)), 
-                 regexp = "is\\(df, \"data.frame\"\\) is not TRUE")
+                 regexp = 'is(df, "data.frame") is not TRUE',
+                 fixed = TRUE)
     
     ## methodCol
     expect_error(bettr(df = df, methodCol = 1), 
-                 regexp = "is\\(methodCol, \"character\"\\) is not TRUE")
+                 regexp = 'is(methodCol, "character") is not TRUE',
+                 fixed = TRUE)
     expect_error(bettr(df = df, methodCol = c("x", "y")), 
-                 regexp = "length\\(methodCol\\) == 1 is not TRUE")
+                 regexp = 'length(methodCol) == 1 is not TRUE',
+                 fixed = TRUE)
     expect_error(bettr(df = df, methodCol = "Missing"), 
-                 regexp = "methodCol %in% colnames\\(df\\) is not TRUE")
+                 regexp = 'methodCol %in% colnames(df) is not TRUE',
+                 fixed = TRUE)
     
     ## metricCol
     expect_error(bettr(df = df, metricCol = 1), 
-                 regexp = "is\\(metricCol, \"character\"\\) is not TRUE")
+                 regexp = 'is(metricCol, "character") is not TRUE',
+                 fixed = TRUE)
     expect_error(bettr(df = df, metricCol = c("x", "y")), 
-                 regexp = "length\\(metricCol\\) == 1 is not TRUE")
+                 regexp = 'length(metricCol) == 1 is not TRUE',
+                 fixed = TRUE)
     expect_error(bettr(df = df, metricCol = "Missing"), 
-                 regexp = "metricCol %in% colnames\\(df\\) is not TRUE")
+                 regexp = 'metricCol %in% colnames(df) is not TRUE',
+                 fixed = TRUE)
     
     ## valueCol
     expect_error(bettr(df = df, valueCol = 1), 
-                 regexp = "is\\(valueCol, \"character\"\\) is not TRUE")
+                 regexp = 'is(valueCol, "character") is not TRUE',
+                 fixed = TRUE)
     expect_error(bettr(df = df, valueCol = c("x", "y")), 
-                 regexp = "length\\(valueCol\\) == 1 is not TRUE")
+                 regexp = 'length(valueCol) == 1 is not TRUE',
+                 fixed = TRUE)
     expect_error(bettr(df = df, valueCol = "Missing"), 
-                 regexp = "valueCol %in% colnames\\(df\\) is not TRUE")
+                 regexp = 'valueCol %in% colnames(df) is not TRUE',
+                 fixed = TRUE)
     df$Value2 <- as.character(df$Value)
     expect_error(bettr(df = df, valueCol = "Value2"), 
-                 regexp = "is\\(df\\[\\[valueCol\\]\\], \"numeric\"\\) is not TRUE")
+                 regexp = 'is(df[[valueCol]], "numeric") is not TRUE',
+                 fixed = TRUE)
     df$Value2 <- NULL
     
     ## Forbidden column names
     df$Score <- seq_len(nrow(df))
     expect_error(bettr(df = df), 
-                 regexp = "!\\(\"Score\" %in% colnames\\(df\\)\\) is not TRUE")
+                 regexp = '!("Score" %in% colnames(df)) is not TRUE',
+                 fixed = TRUE)
     df$Score <- NULL; df$Weight <- seq_len(nrow(df))
     expect_error(bettr(df = df), 
-                 regexp = "!\\(\"Weight\" %in% colnames\\(df\\)\\) is not TRUE")
+                 regexp = '!("Weight" %in% colnames(df)) is not TRUE',
+                 fixed = TRUE)
     df$Weight <- NULL; df$ScaledValue <- seq_len(nrow(df))
     expect_error(bettr(df = df), 
-                 regexp = "!\\(\"ScaledValue\" %in% colnames\\(df\\)\\) is not TRUE")
+                 regexp = '!("ScaledValue" %in% colnames(df)) is not TRUE',
+                 fixed = TRUE)
     df$ScaledValue <- NULL
     
     ## Initial weights
     expect_error(bettr(df = df, initialWeights = 1),
-                 regexp = "is.null\\(initialWeights\\)")
+                 regexp = 'is.null(initialWeights)',
+                 fixed = TRUE)
     expect_error(bettr(df = df, initialWeights = c(1, 2, 3)),
-                 regexp = "is.null\\(initialWeights\\)")
+                 regexp = 'is.null(initialWeights)',
+                 fixed = TRUE)
     expect_error(bettr(df = df, initialWeights = c(A = 1, B = 2, D = 3)),
-                 regexp = "is.null\\(initialWeights\\)")
+                 regexp = 'is.null(initialWeights)',
+                 fixed = TRUE)
     expect_error(bettr(df = df, initialWeights = c(A = 0, B = 0, C = 0)),
-                 regexp = "is.null\\(initialWeights\\)")
+                 regexp = 'is.null(initialWeights)',
+                 fixed = TRUE)
 })
 
 test_that("bettr runs with valid inputs", {
