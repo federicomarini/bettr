5dff84d4d38b33801bd1543aef350c5b489e27f7
Charlotte Soneson <charlottesoneson@gmail.com>
2025-09-22 18:36:49 +0200

    Linting fixes, R/generateColors.R

--- R/generateColors.R
+++ R/generateColors.R
@@ -1,39 +1,39 @@
 #' Generate random colors for annotations
-#' 
+#'
 #' @param df A `data.frame` with annotations in columns.
-#' @param inputColors A list with predefined colors for some of the 
+#' @param inputColors A list with predefined colors for some of the
 #'     annotations, or `NULL`.
-#' @param ggplot2Columns Names of columns for which we should use the standard 
+#' @param ggplot2Columns Names of columns for which we should use the standard
 #'     ggplot2 colors if nothing else is specified
-#' 
+#'
 #' @returns A list with color vectors for all annotations.
-#' 
+#'
 #' @keywords internal
 #' @noRd
-#' 
+#'
 #' @importFrom circlize colorRamp2
 #' @importFrom grDevices colors
-#' 
-.generateColors <- function(df, inputColors, ggplot2Columns = c()) {
+#'
+.generateColors <- function(df, inputColors, ggplot2Columns = character(0L)) {
     if (is.null(df)) {
         return(NULL)
-    } 
-    
+    }
+
     if (is.null(inputColors)) {
         inputColors <- list()
     }
-    
-    c(inputColors, 
-      lapply(df[, setdiff(colnames(df), c(names(inputColors), ggplot2Columns)), 
+
+    c(inputColors,
+      lapply(df[, setdiff(colnames(df), c(names(inputColors), ggplot2Columns)),
                 drop = FALSE],
              function(x) {
                  if (is.numeric(x)) {
-                     circlize::colorRamp2(range(x), 
-                                          c("white", 
-                                            sample(grDevices::colors(), 1)))
+                     circlize::colorRamp2(range(x),
+                                          c("white",
+                                            sample(grDevices::colors(), 1L)))
                  } else {
-                     structure(sample(grDevices::colors(), length(unique(x))),
-                               names = as.character(unique(x)))
+                     setNames(sample(grDevices::colors(), length(unique(x))),
+                              nm = as.character(unique(x)))
                  }
              }),
       lapply(df[, setdiff(ggplot2Columns, names(inputColors)), drop = FALSE],
@@ -41,26 +41,24 @@
                  if (is.numeric(x)) {
                      stop("Can't use ggplot2 colors for continuous columns")
                  } else {
-                     structure(.gg_color_hue(length(unique(x))),
-                               names = as.character(unique(x)))
+                     setNames(.gg_color_hue(length(unique(x))),
+                              nm = as.character(unique(x)))
                  }
-             })
-    )
+             }))
 }
 
 #' Emulate ggplot2 colors
 #' stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
-#' 
+#'
 #' @param n Numeric scalar, the number of colors to generate.
-#' 
+#'
 #' @returns A length-`n` vector of colors.
-#' 
+#'
 #' @keywords internal
 #' @noRd
-#' 
+#'
 #' @importFrom grDevices hcl
 .gg_color_hue <- function(n) {
-    hues <- seq(15, 375, length = n + 1)
-    grDevices::hcl(h = hues, l = 65, c = 100)[seq_len(n)]
+    hues <- seq(15L, 375L, length = n + 1L)
+    grDevices::hcl(h = hues, l = 65L, c = 100L)[seq_len(n)]
 }
-
